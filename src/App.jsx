65024a50d82db9694ffb6a8b0dd98c2c7914f276
Amir <chelabi.ext@whitebay.limited>
2025-07-09 16:19:57 +0100

    fixed tailwind added greeting message

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,25 @@ import Image from "./Image"
 import Name from "./Name"
 import Price from "./Price"
 
+const firstName  ="";
+
 function App() {
 
   return (
-    
-    <div className="border border-red-700">
-      <div>
-        <Name/>
-        <Price/>
-        <Description/>
-        <Image/>
+    <div className="flex flex-col items-center">
+      <div className="inline-block border-8 border-black">
+        <div className="flex flex-col items-center ">
+          <Name/>
+          <Price/>
+          <Description/>
+          <Image/>
+        </div>
+        
       </div>
-      
+      <p className="mt-4 text-lg font-bold">hello, {firstName ? firstName : "there"}</p>
     </div>
+    
+    
     
       
     
